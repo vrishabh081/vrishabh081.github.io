6f06ef408d97b141ca7ac43904e1153f3cb2a5a8
vrishabh081 <rishabhvishwakarma398@gmail.com>
2023-02-03 12:55:07 +0530

    Added profile-pic

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import Resume from "../Resume/Rishabh_Vishwakarma_Resume.pdf"
+import mypic from  "../Images/mypic.png"
 
 function Home()
 {
@@ -7,7 +8,7 @@ function Home()
             <div id="home">
             <div>
                 <h1>Hello, my name is <br /> <span>Rishabh Vishwakarma</span> <br /> And I'm a <br /> <span>Full Stack Web Developer</span></h1>
-                <img src="https://media-exp1.licdn.com/dms/image/D4D03AQHxgRDn_19_Tw/profile-displayphoto-shrink_800_800/0/1669238511709?e=1675296000&v=beta&t=qeq_DbL6vG597-_Ns2cR98wprOVxPD1DThzDEx3CeRE"  alt="mypic" />
+                <img src={mypic}  alt="mypic" />
             </div>
             <div id="resume-home" >
                 <a href="https://drive.google.com/file/d/1xNRsBOBiUDKEodIGDfb1yw9utrY-OFbk/view?usp=share_link" target={"blank"} download>
