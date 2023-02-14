4f9c7437ccfe3d3ac9e01fc14020b0899af862c1
vrishabh081 <rishabhvishwakarma398@gmail.com>
2023-02-14 10:05:54 +0530

    added projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,8 +4,16 @@ import { useEffect } from "react";
 import Fashion from "../Images/Fashion.png";
 import Espn from "../Images/Espn.png";
 import mywardrobe from "../Images/mywardrobe.png";
-import PluralSight from "../Images/PluralSight.png";
+import StepAdding from "../Images/StepAdding.png";
+import Merchanto from "../Images/Merchanto.png";
 
+let porjects = [
+    {
+        title : "My Wardrobe",
+        description: "This website contains all products related to women fashion",
+        techStack: "ReactJs, Redux, Chakra-UI, JavaScript, HTML and CSS",
+    }
+]
 export function Projects()
 {
     useEffect(()=>
@@ -19,9 +27,37 @@ export function Projects()
                 <h1>My Projects</h1>
                 <div id="projects-items" data-aos="fade-up">
                     <div>
+                        <h3 className="head-name">Merchanto</h3>
+                        <p>This project is clone of Meesho E-commerce site. This site basically belong to all category as it contains all products related to men, women and kid.</p>
+                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : React.js, Javascript, Redux, Node.js, Express.js, MongoDB, Chakra UI, NPM Packages</p>
+                        <div id="icons">
+                            <a href="https://merchanto.netlify.app/"><ion-icon name="eye"></ion-icon></a>
+                            <a href="https://github.com/saurabhsinghmehta/skillful-shirt-5398"><ion-icon name="logo-github"></ion-icon></a>
+                        </div>
+                    </div>
+                    <div id="image-div">
+                        <img src={Merchanto} alt="" />
+                    </div>
+                </div>
+                <div id="projects-items" data-aos="fade-up">
+                    <div>
+                        <h3 className="head-name">Step Adding</h3>
+                        <p>Hi, in this project, we are solving a real life problem. Here, we are calculating carry and printing sum step by step.</p>
+                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : React.js, Javascript, Node.js, Express.js</p>
+                        <div id="icons">
+                            <a href="https://frontend-taupe-six.vercel.app/"><ion-icon name="eye"></ion-icon></a>
+                            <a href="https://github.com/vrishabh081/company_assignment_1"><ion-icon name="logo-github"></ion-icon></a>
+                        </div>
+                    </div>
+                    <div id="image-div">
+                        <img src={StepAdding} alt="" />
+                    </div>
+                </div>
+                <div id="projects-items" data-aos="fade-up">
+                    <div>
                         <h3 className="head-name">My Wardrobe</h3>
                         <p>This website contains all products related to women fashion </p>
-                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : ReactJs, Redux, Chakra-UI, JavaScript, HTML and CSS</p>
+                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : React.js, Redux, Chakra-UI, JavaScript, HTML and CSS</p>
                         <div id="icons">
                             <a href="https://clever-nougat-a72f71.netlify.app/"><ion-icon name="eye"></ion-icon></a>
                             <a href="https://github.com/mohitd427/erratic-dolls-5259"><ion-icon name="logo-github"></ion-icon></a>
@@ -31,12 +67,11 @@ export function Projects()
                         <img src={mywardrobe} alt="" />
                     </div>
                 </div>
-
                 <div id="projects-items" data-aos="fade-up">
                     <div>
                         <h3 className="head-name">Fashion Collection</h3>
                         <p>A fashion collection website where you can find and purchase clothing items</p>
-                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : ReactJs, JavaScript, HTML and CSS</p>
+                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : React.js, JavaScript, HTML and CSS</p>
                         <div id="icons">
                             <a href="https://fashion-collection.vercel.app/"><ion-icon name="eye"></ion-icon></a>
                             <a href="https://github.com/vrishabh081/important-fog-2987"><ion-icon name="logo-github"></ion-icon></a>
@@ -58,20 +93,6 @@ export function Projects()
                     </div>
                     <div id="image-div">
                         <img src={Espn} alt="" />
-                    </div>
-                </div>
-                <div id="projects-items" data-aos="fade-up">
-                    <div>
-                        <h3 className="head-name">Plural Sight</h3>
-                        <p>An Education website where a lot of courses are available like web designing, digital market and many more.</p>
-                        <p style={{marginTop:"15px", fontSize:"17px"}}> <span className="tech-stack">Tech Stack</span> : JavaScript, HTML and CSS</p>
-                        <div id="icons">
-                            <a href="https://fashion-collection.vercel.app/"><ion-icon name="eye"></ion-icon></a>
-                            <a href="https://github.com/vrishabh081/important-fog-2987"><ion-icon name="logo-github"></ion-icon></a>
-                        </div>
-                    </div>
-                    <div id="image-div">
-                        <img src={PluralSight} alt="" />
                     </div>
                 </div>
             </div>
